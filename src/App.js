dbdbe8ba8ad0dad6756a5c784e67a4bb9f3fb961
natalie <sachua.2020@smu.edu.sg>
2023-04-17 14:31:30 +0800

    dd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Button, FormControl, MenuItem, Select } from "@mui/material";
 import Flashcard from "./Flashcard";
 import Carousel from 'react-material-ui-carousel';
+import { useState } from "react";
 
 function App() {
+  const [dropdownValue, setDropdownValue] = useState("All");
+
+  function handleChange(e) {
+    setDropdownValue(e.target.value)
+}
 
   let flashcards = [{
     question: "Question 1",
@@ -16,6 +23,8 @@ function App() {
     answer: "hello \n hello \n hello \n hello \n hello \n hello \n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello\n hello"
   }]
 
+  let categories = ["Category 1", "Category 2", "Category 3"]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +32,32 @@ function App() {
         <p>
           Flashcard
         </p>
+
+        <div className="dropdownContainerDiv" style={{display: "flex"}}>
+            <div className="dropdown">
+                <FormControl sx={{width: "200px"}}>
+                    <Select value={dropdownValue} 
+                            onChange={(e) => handleChange(e)}>
+                        <MenuItem value="All">All</MenuItem>
+                        {
+                            categories.map((category) => {
+                                return (
+                                    <MenuItem value={category}>{category}</MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                </FormControl>
+            </div>
+            <Button variant="contained" 
+                sx={{ 
+                    backgroundColor: "#41ADA4",
+                    ':hover': {
+                        bgcolor: '#41ADA4'
+                    }
+                }}>Search</Button>
+            
+        </div>
         
         <div sx={{width: "600px"}}>
           <Carousel swipe={false}
